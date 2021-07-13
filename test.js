bf53d5ce9ff3161e44359a72dd77b94654045cda
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 13:47:45 +0100

    basics 5

--- test.js
+++ test.js
@@ -6,5 +6,6 @@
 // const int = setInterval(() =>{
 //     console.log("in the interval");
 // },1000);
-console.log(_dirname);
-console.log(_filname);
+// console.log(_dirname);
+// console.log(_filname);
+console.log(document.querySelector);
