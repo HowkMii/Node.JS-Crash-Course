5b48ee0579212e84396e3c8f61c3da16f523e515
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 14:19:32 +0100

    basics 9

--- file.js
+++ file.js
@@ -1,9 +1,15 @@
-const fs = require('fs');
-//reading file
-fs.readFile('./docs/test.txt',(err, data)=>{
-    if (err){
-        console.log(err);
-    }
-    console.log(data.toString());
+// const fs = require('fs');
+// //reading file
+// fs.readFile('./docs/test.txt',(err, data)=>{
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
+// console.log('last line')
+
+//writing file
+fs.writeFile('./docs/test.txt','hello kirakom',() =>{
+    console.log('file was written');
 });
-console.log('last line')
+
