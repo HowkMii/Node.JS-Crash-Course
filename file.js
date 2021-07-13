e2c6c70ec48286b3b94a21328f739c95ae1f7c08
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 14:21:59 +0100

    basics 10

--- file.js
+++ file.js
@@ -9,7 +9,14 @@
 // console.log('last line')
 
 //writing file
-fs.writeFile('./docs/test.txt','hello kirakom',() =>{
-    console.log('file was written');
-});
+// fs.writeFile('./docs/test.txt','hello kirakom',() =>{
+//     console.log('file was written');
+// });
+//directories
+fs.mkdir('.assets', (err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log('folder created')
+})
 
