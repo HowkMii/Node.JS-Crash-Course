69907ad076bcbd0ae201ddb1b5429e7cc22061f9
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 14:06:57 +0100

    basics 7

--- modules.js
+++ modules.js
@@ -1,1 +1,2 @@
-const xyz = require('./people');
+const {people, age} = require('./people');
+console.log(people, age);
--- people.js
+++ people.js
@@ -1,2 +1,6 @@
 const people = ['dai','hakim'];
+const age =[22,23];
 console.log(people);
+module.exports = {
+    people, age
+};
