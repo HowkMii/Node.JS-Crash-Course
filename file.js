82d33316588de6c5f004aa4e83c802ac430d7455
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 14:14:18 +0100

    basics 8

---
+++ file.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+//reading file
+fs.readFile('./docs/test.txt',(err, data)=>{
+    if (err){
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+console.log('last line')
