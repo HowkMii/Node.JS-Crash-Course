a664035f49ead308925a43a8898cabdcaa34c725
unknown <abdelhakim@univ-tiaret.dz>
2021-07-13 13:46:24 +0100

    basics 3

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-setTimeout(() => {
-    console.log('in the time')
-    clearInterval(int);
-}, 3000);
+// setTimeout(() => {
+//     console.log('in the time')
+//     clearInterval(int);
+// }, 3000);
 
-const int = setInterval(() =>{
-    console.log("in the interval");
-},1000);
+// const int = setInterval(() =>{
+//     console.log("in the interval");
+// },1000);
+console.log(_dirname);
+console.log(_filname);
