d5cd5e613a59e10b8e6b010cfdc122222af5fe6a
unknown <abdelhakim@univ-tiaret.dz>
2021-07-14 12:39:29 +0100

    creating a server

--- modules.js
+++
@@ -1,2 +0,0 @@
-const {people, age} = require('./people');
-console.log(people, age);
--- people.js
+++
@@ -1,6 +0,0 @@
-const people = ['dai','hakim'];
-const age =[22,23];
-console.log(people);
-module.exports = {
-    people, age
-};
