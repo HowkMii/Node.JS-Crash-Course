6a893bf423968a5db851f6888ab19553e0c03df4
unknown <abdelhakim@univ-tiaret.dz>
2021-07-16 09:03:03 +0100

    reponse object

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http= require('http');
+const fs = require('fs');
 const server = http.createServer((req,res)=> {
 
     console.log(req.url, req.method);
     res.setHeader('Content-Type','text/html');
-    res.write('<h1>hello again</h1>');
-    res.end();
+    fs.readFile('./views/index.html',(err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
 });
 
 server.listen(3000,'localhost',()=>{
