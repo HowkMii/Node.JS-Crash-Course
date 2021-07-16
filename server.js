d75b0941baf2320cfb6c19fd764786ac4ca74681
unknown <abdelhakim@univ-tiaret.dz>
2021-07-16 09:14:39 +0100

    basic routing

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const server = http.createServer((req,res)=> {
 
     console.log(req.url, req.method);
     res.setHeader('Content-Type','text/html');
+    let path = './views';
+    switch(req.url){
+        case'/':
+            path +='index.html';
+            break;
+        case '/about':
+            path +='about.html';
+            break;
+        case '/404':
+            path +='404.html';
+            break;
+    }
     fs.readFile('./views/index.html',(err, data)=>{
         if(err){
             console.log(err);
