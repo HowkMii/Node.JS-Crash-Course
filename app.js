beff34369f53576a89ef996a149caa18acdb1870
unknown <abdelhakim@univ-tiaret.dz>
2021-07-18 14:01:30 +0100

    routing and htmlpages

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+//   res.send('Hello World')
+res.sendFile('./views/index.html',{root: __dirname});
 });
  
+app.get('/about', function (req, res) {
+    // res.send('about')
+    res.sendFile('./views/about.html',{root: __dirname});
+  });
+app.get('/404', function (req, res) {
+    // res.send('about')
+    res.sendFile('./views/404.html',{root: __dirname});
+  });
 app.listen(3000);
