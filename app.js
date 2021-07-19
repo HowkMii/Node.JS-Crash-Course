c9a70d6dec250b570130c7a3b6426d3cb9bce048
unknown <abdelhakim@univ-tiaret.dz>
2021-07-19 05:12:12 +0100

    ejs view engine3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine','ejs');
  
 app.get('/', function (req, res) {
 //   res.send('Hello World')
-res.sendFile('./views/index.html',{root: __dirname});
+res.render('index')
 });
  
 app.get('/about', function (req, res) {
