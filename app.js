1056d835e10c5a7a0dfc034f9d44abdfc8c22646
unknown <abdelhakim@univ-tiaret.dz>
2021-07-19 05:24:58 +0100

    finish

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const app = express();
-//register view engine 
-app.set('view engine','ejs');
 
- 
-app.get('/', function (req, res) {
-//   res.send('Hello World')
-res.render('index')
-});
- 
-app.get('/about', function (req, res) {
-    // res.send('about')
-    res.render('About')
-  });
-app.get('/about-us', function (req, res) {
-    // res.send('about')
-    res.redirect('./about');
-  });
-app.use((req,res)=>{
-    res.status(404).render('404');
-})
+// express app
+const app = express();
+
+// listen for requests
 app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
