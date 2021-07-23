107d470bafe1c170dbe66a662167404c229c4e13
unknown <abdelhakim@univ-tiaret.dz>
2021-07-23 14:44:51 +0100

    mogan

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
-
+const morgan = require('morgan');
 // express app
 const app = express();
 
+
 // listen for requests
 app.listen(3000);
-app.use((req, res, next)=>{
-  console.log('new request made: ');
-  console.log('host:', req.hostname);
-  console.log('path :', req.path);
-  console.log('mrthod :' ,req.mrthod);
-  next();
-});
-app.use((req, res, next)=>{
-  console.log('in the next middleware: ');
-  
-  next();
-});
+app.use(morgan('dev'));
 // register view engine
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
