2e372a7d87bf4495fe16fecd99665e966239fb9d
howkmii <abdelhakim@univ-tiaret.dz>
2021-07-25 19:15:22 +0100

    conected to db

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 // express app
 const app = express();
-
+const dbURI = "mongodb+srv://howkmii:Artist@cluster0.kmd1a.mongodb.net/note-tuts?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
 // listen for requests
-app.listen(3000);
+
+app.use(express.static('public'));
 app.use(morgan('dev'));
 // register view engine
 app.set('view engine', 'ejs');
